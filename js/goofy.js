4abdfb0fd89ed26961229299b2740405a9581642
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-11 00:19:21 -0300

    remove unecessary line

--- js/goofy.js
+++ js/goofy.js
@@ -1,7 +1,6 @@
+const modal = document.querySelector("[data-modal]");
 const howaboutno = document.querySelector("[data-how-about]");
 const notwork = document.querySelector("[data-work]");
-const modal = document.querySelector("[data-modal]");
-const close = document.querySelector(".close");
 const aintgonwork = document.querySelector("[data-not-gonna-work]");
 
 howaboutno.addEventListener("click", (e) => {
